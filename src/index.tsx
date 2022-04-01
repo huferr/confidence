a0f0e1ed280b3c5b598ac60a43449e520e5f736b
Hugo Ferreira <hugoferreirait@gmail.com>
2022-04-01 16:18:57 -0300

    fix: changing to react 18 render api

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import * as ReactDOMClient from 'react-dom/client';
+import './styles/globalStyles.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+const root = ReactDOMClient.createRoot(container as HTMLElement);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
+
