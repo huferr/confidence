5806fc5ca3e3bf95514440e861dfdce9730f857b
Hugo Ferreira <hugoferreirait@gmail.com>
2022-04-01 16:19:11 -0300

    feat: response typing

---
+++ src/typings/response.ts
@@ -0,0 +1,29 @@
+interface Location {
+    active: boolean,
+    address: {
+        addressLine1: string,
+        addressLine2: string,
+        city: string,
+        state: string,
+        zip: string
+    }
+    id: number,
+    latitude: number,
+    locationDetails: string,
+    locationId: string,
+    locationName: string,
+    locationType: string,
+    locationUserRole: string,
+    longitude: number,
+    newLocation: boolean,
+    numberofDevices: number,
+    subscriptionActive: boolean,
+}
+
+
+export interface IResponse {
+    data: {
+      locations: Location[],
+      numberOfLocations: number,
+    }
+}
