97dc35dcd4002179fbd2821beadab46505b2b0aa
Hugo Ferreira <hugoferreirait@gmail.com>
2022-04-02 18:33:07 -0300

    chore: 2 tabs indentation

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://6246bd8e739ac8459191f7d5.mockapi.io/v2',
+	baseURL: 'https://6246bd8e739ac8459191f7d5.mockapi.io/v2',
 })
--- src/services/locations.service.ts
+++ src/services/locations.service.ts
@@ -2,11 +2,11 @@ import { IResponse } from "../typings/response";
 import { api } from "./api";
 
 export const getLocations = async (): Promise<IResponse> => {
-    try {
-       const response = await api.get('/confidence/locations')
-        return response;
-    } catch (error: any) {
-        return error;
-    }
+	try {
+	  const response = await api.get('/confidence/locations')
+		return response;
+	} catch (error: any) {
+		return error;
+	}
 }
 
