2f784f9297ccec80779c02b8f9cdd9c64d962d93
Hugo Ferreira <hugoferreirait@gmail.com>
2022-04-02 18:32:01 -0300

    feat: location type and address icons

---
+++ src/assets/icons/locationTypeIcon.tsx
@@ -0,0 +1,11 @@
+
+export const LocationTypeIcon = () => (
+	<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path d="M13.8085 2.95813H6.57498C6.09536 2.95813 5.6354 3.14865 5.29626 3.48779C4.95713 3.82693 4.7666 4.28689 4.7666 4.7665V19.2335C4.7666 19.7131 4.95713 20.1731 5.29626 20.5122C5.6354 20.8513 6.09536 21.0419 6.57498 21.0419H17.4252C17.9048 21.0419 18.3648 20.8513 18.7039 20.5122C19.0431 20.1731 19.2336 19.7131 19.2336 19.2335V8.38325L13.8085 2.95813Z" stroke="#757893" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+		<path d="M13.8085 2.95813V8.38325H19.2336" stroke="#757893" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+		<path d="M15.6168 12.9043H8.3833" stroke="#757893" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+		<path d="M15.6168 16.521H8.3833" stroke="#757893" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+		<path d="M10.1917 9.28735H9.28749H8.3833" stroke="#757893" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+	</svg>
+)
+
