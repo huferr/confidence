953c95e3ae79cf3a1e18f0076020c8f05b59da31
Hugo Ferreira <hugoferreirait@gmail.com>
2022-04-04 11:59:37 -0300

    fix: header test suit name

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import "@testing-library/jest-dom/extend-expect"
 import { Header } from '.'
 
-describe('LocationCard Component Test Suites', () => {
+describe('Header Component Test Suites', () => {
   it('has correct text wrapped in heading', () => {
     render(<Header />)
     expect(screen.getByLabelText('heading')).toHaveTextContent('Confidence System Challenge')
