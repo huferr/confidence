d940db7e288d36197c2793b12fc0aef47d5117aa
Hugo Ferreira <hugoferreirait@gmail.com>
2022-04-04 13:08:15 -0300

    chore: tab size config

--- src/components/Header/Header.component.tsx
+++ src/components/Header/Header.component.tsx
@@ -4,8 +4,8 @@ import './Header.styles.css'
 export const Header = () => {
 	return (
 		<header>
-		  <img src={Logo} alt="Logo" className="logo"/>
-		  <h2 aria-label="heading">Confidence System Challenge</h2>
-	  </header>
+			<img src={Logo} alt="Logo" className="logo" />
+			<h2 aria-label="heading">Confidence System Challenge</h2>
+		</header>
 	)
 }
