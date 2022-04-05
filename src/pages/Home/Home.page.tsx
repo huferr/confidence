652cc192038895ffc99bcf8b836b5a58f86b9be3
Hugo Ferreira <hugoferreirait@gmail.com>
2022-04-05 16:55:31 -0300

    chore: refactor to infinity scroll

--- src/pages/Home/Home.page.tsx
+++ src/pages/Home/Home.page.tsx
@@ -1,29 +1,54 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { getLocations } from '../../services';
-import { LocationCard, Header } from '../../components';
+import { Header, LocationCard } from '../../components';
 import { Location } from '../../typings';
-import { splitArrayBy } from '../../helpers';
 import './Home.styles.css'
 
 export const Home = () => {
-
   const [locations, setLocations] = useState<Location[][]>([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [showLoading, setShowLoading] = useState(true);
+  const loaderRef = useRef(null);
 
   useEffect(() => {
-    (async () => {
-      const { data: { locations } } = await getLocations()
-      setLocations(splitArrayBy(3, locations))
-    })()
+    const options = {
+      root: null,
+      rootMargin: "20px",
+      threshold: 1.0
+    };
+
+    const observer = new IntersectionObserver((entities) => {
+      const target = entities[0];
+      if (target.isIntersecting) setCurrentPage(curr => curr + 3);
+    }, options);
+  
+    if (loaderRef.current) observer.observe(loaderRef.current);
   }, [])
+  
+  useEffect(() => {
+    (async () => {
+      const { data } = await getLocations(currentPage)
+
+      if (!data.locations.length) setShowLoading(false); 
+      else setLocations((prev) => [...prev, [...data.locations]]);
+      
+    })()
+  }, [currentPage]);
 
   return (
     <>
       <Header />
-      {locations.map((group, index) => (
-        <section key={index} className={index === 0 ? 'first-section' : ''}>
-          {group.map((location) => <LocationCard key={location.id} location={location} />)}
-        </section>
-      ))}
+      {console.log(locations)}
+      <main>
+        {locations.map((loc, index) => ( 
+          <section key={index}>
+            {loc.map(i => (
+              <LocationCard key={i.id} location={i} /> 
+            ))}
+          </section>
+          ))}
+      </main>
+      <h3 ref={loaderRef}>{showLoading ? 'Loading more locations...' : 'End!'}</h3>
     </>
   );
 };
--- src/services/locations.test.ts
+++ src/services/locations.test.ts
@@ -5,7 +5,28 @@ jest.mock('axios');
 
 const mockedAxios = jest.mocked(axios, true);
 
-const resultParsed = {
+const resultParsed = [{
+  "id": 95144,
+  "locationId": "HOM49XD6",
+  "locationName": "Non Seq - Amit's workspace",
+  "locationDetails": "",
+  "locationType": "Personal",
+  "numberofDevices": 0,
+  "address": {
+      "addressLine1": "Geng Road",
+      "addressLine2": "",
+      "city": "Palo Alto",
+      "state": "CA",
+      "zip": "94303"
+  },
+  "locationUserRole": "Owner",
+  "active": false,
+  "newLocation": false,
+  "subscriptionActive": false,
+  "longitude": -122.119496,
+  "latitude": 37.451601
+},
+{
   "id": 82809,
   "locationId": "BUS134NE",
   "locationName": "Validate Workspace White Screen",
@@ -13,11 +34,11 @@ const resultParsed = {
   "locationType": "Business",
   "numberofDevices": 0,
   "address": {
-    "addressLine1": "11 Van Ness Avenue",
-    "addressLine2": "",
-    "city": "San Francisco",
-    "state": "CA",
-    "zip": "94102"
+      "addressLine1": "11 Van Ness Avenue",
+      "addressLine2": "",
+      "city": "San Francisco",
+      "state": "CA",
+      "zip": "94102"
   },
   "locationUserRole": "Owner",
   "active": true,
@@ -25,20 +46,52 @@ const resultParsed = {
   "subscriptionActive": false,
   "longitude": -122.418823,
   "latitude": 37.774538
-}
+},
+{
+  "id": 77752,
+  "locationId": "HOMEDJ8B",
+  "locationName": "Report Testing",
+  "locationDetails": "Report Testing",
+  "locationType": "Personal",
+  "numberofDevices": 0,
+  "address": {
+      "addressLine1": "6463 Calle Esperanza",
+      "city": "Pleasanton",
+      "state": "CA",
+      "zip": "94566"
+  },
+  "locationUserRole": "Owner",
+  "active": true,
+  "newLocation": false,
+  "subscriptionActive": false,
+  "longitude": -121.903796,
+  "latitude": 37.671177
+}]
+
+  
 
 it('Should return a location object', async () => {
-  mockedAxios.get.mockImplementation(() => Promise.resolve(resultParsed));
+  mockedAxios.post.mockImplementation(() => Promise.resolve(resultParsed));
 
-  const res = await getLocations();
-  expect(axios.get).toHaveBeenCalledWith('https://6246bd8e739ac8459191f7d5.mockapi.io/v2/confidence/locations');
+  const res = await getLocations(0);
+  expect(axios.post).toHaveBeenCalledWith('/confidence/locations', {
+    "limit": 3, 
+    "start": 0
+  }, 
+  {
+    "headers": {
+      "cache-control": "no-cache",
+      "content-type": "application/json",
+      "username": "amitphatak$r5labs.com"
+    }
+  });
   expect(res).toEqual(resultParsed);
 });
 
 it('Should throw an error', async () => {
-  mockedAxios.get.mockImplementation(() => Promise.reject(new Error('Network Error')));
+  mockedAxios.post.mockImplementation(() => Promise.reject(new Error('Network Error')));
 
-  const res = await getLocations();
+  const res = await getLocations(1);
   expect(res).toStrictEqual(Error('Network Error'));
 });
 
