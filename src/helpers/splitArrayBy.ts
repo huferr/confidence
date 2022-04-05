c8b094d9fea0a4171deacf9108edfd24066f1ba8
Hugo Ferreira <hugoferreirait@gmail.com>
2022-04-05 16:40:36 -0300

    chore: removing unused helper

--- src/helpers/splitArrayBy.ts
+++
@@ -1,5 +0,0 @@
-export const splitArrayBy = (size: number, list: Array<any>) => {
-	if (!list) return 'Invalid array';
-	if (!size) return 'Invalid size';
-	return list.reduce((acc, _curr, i, self) => (i % size) ? acc : [...acc, self.slice(i, i + size)], []);
-}
